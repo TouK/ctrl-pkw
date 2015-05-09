a3ea2ac170c15385edbc4dfffcb49ba8fb9fc626
Tomasz Wielga <tw@touk.pl>
2015-05-09 12:36:25 +0200

    wide open cors filter

---
+++ src/main/java/pl/ctrlpkw/api/filter/CorsFilter.java
@@ -0,0 +1,30 @@
+package pl.ctrlpkw.api.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+
+        String allowedOrigin = Optional.ofNullable(requestContext.getHeaderString("origin"))
+                .orElse(Optional.ofNullable(requestContext.getHeaderString("referer"))
+                                .map(refererHeader -> URI.create(refererHeader).getHost())
+                                .orElse("*")
+                );
+        String allowedHeaders = String.join(", ", requestContext.getHeaders().keySet());
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", allowedOrigin);
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", allowedHeaders);
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
